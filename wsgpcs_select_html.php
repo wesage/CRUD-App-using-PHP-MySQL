aec87ae61f03722508d21b1d35caf5c51a69d89d
mm.chen <wesage@126.com>
2024-08-09 11:34:40 +0800

    wsgpcs_select_html.php,增加下拉列表 wsgpcs_update.php,完善uuid字段名 MySQL字段名,默认不区分大小写 202408091134,wesage,r00005

--- wsgpcs_select_html.php
+++ wsgpcs_select_html.php
@@ -26,15 +26,15 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 结果";
 } */
-
 $who = $_POST['who'];
 $table = $_POST['table'];
 //$table = 'IVT';
-
 // Check if the form is submitted using the POST method
 if (isset($_POST["submit"])) {
-    $table = $_POST['brand'];
+	$table = $_POST['brand'];
 	$sn = $_POST['sn'];
+	echo "Brand:".$table;
+	echo "<br>SN:".$sn;
 	if (empty($sn)) {
 		//echo "All fields are required";
 		echo "Sn fields are required!";
@@ -61,11 +61,12 @@ if (isset($_POST["submit"])) {
 			$rows = array();
 			while ($row = $result->fetch_assoc()) {
 				$rows[] = $row;
+				echo "<br>";
 				echo "<table border='1' align='left'>";
 				echo "<tr> <td>Information</td></tr>";
 				echo "<tr> <td>------------------</td></tr>";
 				echo "<tr> <td>Product: " . $row["product"]. "</td></tr>";
-				echo "<tr> <td>UUID: " . $row["UUID"]. "</td></tr>";
+				echo "<tr> <td>UUID: " . $row["uuid"]. "</td></tr>";
 				echo "<tr> <td>Cfg: " . $row["cfg"]. "</td></tr>";
 				echo "<tr> <td>SN: " . $row["sn"]. "</td></tr>";
 				echo "<tr> <td>Company: " . $row["company"]. "</td></tr>";
@@ -80,11 +81,15 @@ if (isset($_POST["submit"])) {
 				echo "<tr> <td>Express_send: " . $row["express_send"]. "</td></tr>";
 				echo "<tr> <td>------------------</td></tr>";
 				echo "</table>";
+				echo "<br>For more information, Please contact the dealer.";
+				echo "<br>Thank you for your support.";
 			}
 			//echo json_encode($rows);
 		} else {
-			echo "Sorry,we can't find the SN.";
-            echo "<br>Please confirm the SN then try again.";
+			echo "<br>Sorry,we can't find the SN.";
+			echo "<br>Please confirm the SN then try again.";
+			echo "<br>For more information, Please contact the dealer.";
+			echo "<br>Thank you for your support.";
 		}
 	}
 }
@@ -93,51 +98,58 @@ $conn->close();
 
 <!doctype html>
 <html lang="en">
-  
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-      WSTRON SN verify and track system
-    </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-    rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-    crossorigin="anonymous">
-  </head>
-  
-  <body>
-    <style>
-      html, body { background-color: gainsboro; }
-    </style>
-    <div class="container py-5 px-5">
-      <div class="container text-center py-3">
-        <h2>
-          SN verify and track system
-        </h2>
-      </div>
-      <!-- Form with POST method to submit data to PHP -->
-      <form method="post">
-        <div class="mb-3">
-          <label for="name" class="form-label">
-            Brand:
-          </label>
-          <input type="name" class="form-control" name="brand" id="brand" placeholder="Enter Your brand">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>
+            WSTRON SN verify and track system
+        </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <style>
+            html, body { background-color: gainsboro; }
+        </style>
+        <div class="container py-5 px-5">
+            <div class="container text-center py-3">
+                <h2>
+                    SN verify and track system
+                </h2>
+            </div>
+            <!-- Form with POST method to submit data to PHP -->
+            <form method="post">
+                <div class="mb-3">
+                    <label for="name" class="form-label">
+                        Brand:
+                    </label>
+                    <select id="brand" name="brand">
+                        <option value="IVT">
+                            IVT
+                        </option>
+                        <option value="WSTRON">
+                            WSTRON
+                        </option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">
+                        SN:
+                    </label>
+                    <input type="name" class="form-control" name="sn" id="sn" placeholder="Enter Your SN">
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary">
+                    Submit
+                </button>
+            </form>
         </div>
-        <div class="mb-3">
-          <label for="name" class="form-label">
-            SN:
-          </label>
-          <input type="name" class="form-control" name="sn" id="sn" placeholder="Enter Your SN">
-        </div>
-        <button type="submit" name="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous">
-    </script>
-  </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous">
+        </script>
+    </body>
 
 </html>
