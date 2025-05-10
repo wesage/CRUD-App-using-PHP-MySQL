fdd37054c28b48548559c8396b3551f20617c8d1
mm.chen <wesage@126.com>
2025-05-10 19:21:30 +0800

    wsgpcs_insert.php 查询是否存在,添加model条件 sn,model值和类型,从手机app请求传入 解决,sn一样,model不一样的情况,后面录入的型号,无法录入 202505101923,wesage

--- wsgpcs_insert.php
+++ wsgpcs_insert.php
@@ -24,6 +24,7 @@ $step = $_POST['step'];
 $date = $_POST['date'];
 $snimeimeid = $_POST['snimeimeid'];
 $sql = "INSERT INTO $table (`sn`) VALUES ('$sn')";
+//SELECT 1 FROM $table WHERE ((`sn` = '$sn' AND `model` = $model)),'$sn'和$model变量类型不一样
 if($table == 'MTK') {
 	$sql_smt = "INSERT INTO $table (`product`, `cfg`, `sn`, `company`, `hardware`, `model`, `lb_date_smt`, `snimeimeid`)
 SELECT $product, $cfg, '$sn', $company, $hardware, $model, '$date', '$snimeimeid'
@@ -34,7 +35,7 @@ SELECT 1 FROM $table WHERE ((`sn` = '$sn'))
 	$sql_smt = "INSERT INTO $table (`product`, `cfg`, `sn`, `company`, `hardware`, `model`, `lb_date_smt`)
 SELECT $product, $cfg, '$sn', $company, $hardware, $model, '$date'
 WHERE NOT EXISTS(
-SELECT 1 FROM $table WHERE ((`sn` = '$sn'))
+SELECT 1 FROM $table WHERE ((`sn` = '$sn' AND `model` = $model))
 )";
 }
 if($step == 'SMT') {
